8227f87d516cfc6ea258203a68854389ba5708db
Nicanor ZINKO <parmenaszinko@gmail.com>
2021-08-02 12:27:27 +0000

    Add sessions controller and config this routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   mount LetterOpenerWeb::Engine, at: '/inbox' if Rails.env.development?
 end
