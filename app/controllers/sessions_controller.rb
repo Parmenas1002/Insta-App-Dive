9f9de060389f0df6d4fae2e2ae7d952a1dd38a73
Nicanor ZINKO <parmenaszinko@gmail.com>
2021-08-04 21:08:29 +0000

    Test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
-  def new
+   def new
   end
   def create
     user = User.find_by(email: params[:session][:email])
