0d004af3efc627f08d5a4d5460149b9a277afac2
Nicanor ZINKO <parmenaszinko@gmail.com>
2021-08-04 20:26:02 +0000

    Align indents with sngle-byte spaces

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,10 @@
 class FavoritesController < ApplicationController
-    def create
-        favorite = current_user.favorites.create(post_id: params[:post_id])
-        redirect_to posts_path, notice: "You add #{favorite.post.user.name} post to your favorites posts"
-    end
-    def destroy
-        favorite = current_user.favorites.find_by(id: params[:id]).destroy
-        redirect_to posts_path, notice: "You delete #{favorite.post.user.name} post to your favorites posts"
-    end
+  def create
+    favorite = current_user.favorites.create(post_id: params[:post_id])
+    redirect_to posts_path, notice: "You add #{favorite.post.user.name} post to your favorites posts"
+  end
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to posts_path, notice: "You delete #{favorite.post.user.name} post to your favorites posts"
+  end
 end
